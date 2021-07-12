b14370aed702aa79d4c7f17646ae641e1f3c4ab4
Robert Sheehan <robertnsheehan@gmail.com>
2021-07-12 16:42:05 +0100

    Added sketches for measuring Vout from AM

--- Analog_Out_Test/Analog_Out_Test.ino
+++ Analog_Out_Test/Analog_Out_Test.ino
@@ -1,30 +1,21 @@
-// With a view to implementing a DC measurements using the Arduino Micro test and evaluate the operation of the board
-// Keep commands consistent with SigGen Sketch
-// R. Sheehan 16 - 11 - 2020
-
+// Sketch for testing analog output capabilities from PWM pins
 // For queries on any of the Arduino commands see https://www.arduino.cc/reference/en/
+// R. Sheehan 12 - 7 - 2021
 
 // Constants used in the sketch
 int loud = 0; // boolean needed for printing comments, debug commands etc, loud = 1 => print
-unsigned long delay_val = 3000; // delay value in units of ms
-
-float incomingByte = 0; // Variable to read in desire frequency from serial monitor
-int deleteRead = 0; // Variable to read in variable so it is deleted from the buffer. Not used. 
-
-float VOFF = 1.65; // This is the DC offset applied to the output of the SparkFun MiniGen VOFF = Vin/2, Vin = 3.3V 
-float VMIN = 0.0; // This is the min DC value that can be handled by the Arduino Micro
-float VMAX = 5.0; // This is the max DC value that can be handled by the Arduino Micro 
-float FMIN = 100.0; // Minimum frequency setting for the board, it can actually go lower than this but I'm setting 100 Hz as lower limit
-float FDEFAULT = 1000.0; // Default frequency of board after setup, board will default to f = 100 Hz after calling gen.reset
-float FMAX = 3000000.0; // Maximum frequency setting for the board 3 MHz
-int PLACES = 3; // Output voltage readings to the nearest millivolt
-
+int deleteRead = 0; // Variable to read in variable so it is deleted from the buffer.
 int DCPINA = 9; // pin for outputting DC Values, it is a digital pin adapted for the purpose
 int DCPINB = 6; // pin for outputting DC Values, it is a digital pin adapted for the purpose
+int PLACES = 3; // Output voltage readings to the nearest millivolt
 
-// Commands defined here should be consistent with the AC test code
-const char readCmdStr = 'r'; // read data command string for reading max AC input
-const char writeCmdStr = 'w'; // write data command string for writing frequency values
+unsigned long delay_val = 500; // delay value in units of ms
+
+float incomingByte = 0; // Variable to read in desire frequency from serial monitor
+float VMIN = 0.0; // This is the min DC value that can be handled by the Arduino Micro
+float VMAX = 5.0; // This is the max DC value that can be handled by the Arduino Micro 
+
+// Commands to be sent to AM via serial console
 const char writeAngStrA = 'a'; // write analog output from DCPINA
 const char writeAngStrB = 'b'; // write analog output from DCPINB
 const char readAngStr = 'l'; // read analog input
@@ -33,7 +24,7 @@ String ERR_STRING = "Error: Cannot parse input correctly"; // error message
 
 void setup() {
   // put your setup code here, to run once:
-
+  
   pinMode(DCPINA, OUTPUT); 
   pinMode(DCPINB, OUTPUT);
   pinMode(A0, INPUT); 
@@ -44,7 +35,7 @@ void setup() {
   pinMode(A5, INPUT);  
   
   Serial.begin(9600); // Set up serial monitor at 9600 BAUD, BAUD can probably increase
-  delay(delay_val); // Delay for opening the serial monitor 
+  delay(delay_val); // Delay for opening the serial monitor
 }
 
 void loop() {
@@ -57,13 +48,13 @@ void loop() {
     // Loop checks for characters entered into serial monitor. 
     // Characters must be typed in bar and then the "enter" button must be pressed.
     // for details see https://www.arduino.cc/reference/en/language/functions/communication/serial/available/
-    if (Serial.available() > 0) { //If there are characters (data) in the buffer
+    if (Serial.available() > 0) { // If there are characters (data) in the buffer
   
       // read the command that has been input
       // then tell the board to take appropriate action
-      // either read some voltage values or write a frequency
-      //Serial.println("Write something"); 
-      String input = Serial.readString(); //  
+      // either read some voltage values or write some voltage values from DCPINA or DCPINB
+      
+      String input = Serial.readString(); 
       deleteRead = Serial.read();  // read the incoming byte again to delete it from the buffer as the above command does not delete the data.
       if(loud){
         Serial.println("You have entered: "); 
@@ -79,16 +70,11 @@ void loop() {
         
         if(input[0] == writeAngStrA){ // write analog output on one of pin 3, 5, 6, 9, 10, 11, 13
             // output on DCPINA is followed by RC LPF comprising R = 1 kOhm, C = 100 uF
-            // the idea is that you would read a voltage value from the input stream write out that value using PWM on one of the analog pins
-            // except this isn't what PWM does. PWM does not vary amplitude only pulse duty cycle, however, by varying the pulse duty cycle length
-            // I can alter the average voltage that is being output by the arduino micro. The question is then if that is good enough to characterise a 
-            // diode and a BJT? We shall see. 
-            
+                        
             input.remove(0,1); // remove the write analog command from the start of the string
             // VMIN = 0 V, VMAX = 5 V
             float Vout = min( max( VMIN, input.toFloat() ), VMAX ); // save the output value, force it to output between limits
             int DC =  int( 51 * Vout ); // compute duty cycle based on DC = 255*(Vout/5) = 51 * Vout
-            //int pin = 9; 
   
             if(loud){
               Serial.println("Perform steps necessary for write analog command"); 
@@ -102,22 +88,16 @@ void loop() {
             }
   
             analogWrite(DCPINA, DC); // variable length duty cycle whose average output is Vout
-            delay(delay_val); // give the device time to settle at its new voltage
-  
-            //analogWrite(pin, 0); // always off
-            //analogWrite(pin, 64); // 25% duty cycle
-            //analogWrite(pin, 127); // 50% duty cycle
-            //analogWrite(pin, 191); // 75% duty cycle
-            //analogWrite(pin, 255); // 100% duty cycle
-            //delay(delay_val);       
+            delay(delay_val); // give the device time to settle at its new voltage  
         }
         else if(input[0] == writeAngStrB){ // write analog output on one of pin 3, 5, 6, 9, 10, 11, 13
-            // output on DCPINB is followed by RC LPF comprising R = 1 kOhm, C = 100 uF        
+            // output on DCPINB is followed by RC LPF comprising R = 1 kOhm, C = 100 uF      
+              
             input.remove(0,1); // remove the write analog command from the start of the string
+            
             // VMIN = 0 V, VMAX = 5 V
             float Vout = min( max( VMIN, input.toFloat() ), VMAX ); // save the output value, force it to output between limits
             int DC =  int( 51 * Vout ); // compute duty cycle based on DC = 255*(Vout/5) = 51 * Vout
-            //int pin = 9; 
   
             if(loud){
               Serial.println("Perform steps necessary for write analog command"); 
@@ -192,7 +172,7 @@ float analogVoltageRead(int pin)
 
   if(c3){
     int sensorValue = analogRead(pin); 
-    float n_bits = 1023;  
+    float n_bits = 1023.0;  
     float Vmax = 5.0; // This assumes a 5V reference for the board, this is applicable for the Arduino Micro
     //float Vmax = 3.3; // This assumes a 3.3V reference for the board, this is applicable for the ItsyBitsyM4
     float voltage = sensorValue*(Vmax / n_bits); 
@@ -201,34 +181,5 @@ float analogVoltageRead(int pin)
   }
   else{
     return 0.0;   
-  }
-}
-
-void analogVoltageWrite(int pin, float voltage)
-{
-  // function for converting a voltage value to bit value for analogWrite
-  // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
-  // R. Sheehan 4 - 11 - 2020
-
-  // function can only be used for ItsyBitsyM4
-  // not applicable for Arduino Micro
-
-  bool c1 = pin == A0;
-  bool c2 = pin == A1; 
-  bool c3 = c1 || c2; // must be A0 or A1 to write out voltage using ItsyBitsyM4
-  bool c4 = voltage >= 0.0; // voltage value must be in range
-  bool c5 = voltage < 3.3; 
-  bool c10 = c3 && c4 && c5; 
-
-  if(c10){
-    float n_bits = 1023;  
-    //float Vmax = 5.0; // This assumes a 5V reference for the board, this is applicable for the Arduino Micro
-    float Vmax = 3.3; // This assumes a 3.3V reference for the board, this is applicable for the ItsyBitsyM4
-    int writeVal = int(((4.0*n_bits)/Vmax)*voltage);
-
-    analogWrite(pin, writeVal);   
-  }
-  else{
-    analogWrite(A0, 0);
   }
 }
