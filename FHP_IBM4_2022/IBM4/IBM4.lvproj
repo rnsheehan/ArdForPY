c5533c361637e881c759f7553f88fe0dab9a586c
Robert Sheehan <robertnsheehan@gmail.com>
2022-10-23 14:42:43 +0100

    Added FHP IBM4 code to repository
    FHP version of IBM4 code along with LabVIEW library for the interface

---
+++ FHP_IBM4_2022/IBM4/IBM4.lvproj
@@ -0,0 +1,87 @@
+﻿<?xml version='1.0' encoding='UTF-8'?>
+<Project Name="Template - Generic.lvproj" Type="Project" LVVersion="21008000" URL="/&lt;instrlib&gt;/_Template - Generic/Template - Generic.lvproj">
+	<Property Name="Instrument Driver" Type="Str">True</Property>
+	<Property Name="NI.Project.Description" Type="Str">This project is used by developers to edit API and example files for LabVIEW Plug and Play instrument drivers.</Property>
+	<Item Name="My Computer" Type="My Computer">
+		<Property Name="CCSymbols" Type="Str">OS,Win;CPU,x86;</Property>
+		<Property Name="NI.SortType" Type="Int">3</Property>
+		<Property Name="specify.custom.address" Type="Bool">false</Property>
+		<Item Name="IBM4.lvlib" Type="Library" URL="/&lt;instrlib&gt;/IBM4/IBM4.lvlib"/>
+		<Item Name="Dependencies" Type="Dependencies">
+			<Item Name="vi.lib" Type="Folder">
+				<Item Name="VISA Configure Serial Port (Instr).vi" Type="VI" URL="/&lt;vilib&gt;/Instr/_visa.llb/VISA Configure Serial Port (Instr).vi"/>
+				<Item Name="VISA Configure Serial Port (Serial Instr).vi" Type="VI" URL="/&lt;vilib&gt;/Instr/_visa.llb/VISA Configure Serial Port (Serial Instr).vi"/>
+				<Item Name="VISA Configure Serial Port" Type="VI" URL="/&lt;vilib&gt;/Instr/_visa.llb/VISA Configure Serial Port"/>
+			</Item>
+		</Item>
+		<Item Name="Build Specifications" Type="Build">
+			<Item Name="My Installer" Type="Installer">
+				<Property Name="Destination[0].name" Type="Str">IBM4</Property>
+				<Property Name="Destination[0].parent" Type="Str">{3912416A-D2E5-411B-AFEE-B63654D690C0}</Property>
+				<Property Name="Destination[0].tag" Type="Str">{E49E039F-93CE-447C-BA62-4C096924AC7C}</Property>
+				<Property Name="Destination[0].type" Type="Str">userFolder</Property>
+				<Property Name="DestinationCount" Type="Int">1</Property>
+				<Property Name="DistPart[0].flavorID" Type="Str">DefaultFull</Property>
+				<Property Name="DistPart[0].productID" Type="Str">{1FD331BB-AC24-4120-A8EF-DC2078AD4843}</Property>
+				<Property Name="DistPart[0].productName" Type="Str">NI LabVIEW Runtime 2021 (64-bit)</Property>
+				<Property Name="DistPart[0].SoftDep[0].exclude" Type="Bool">false</Property>
+				<Property Name="DistPart[0].SoftDep[0].productName" Type="Str">NI ActiveX Container (64-bit)</Property>
+				<Property Name="DistPart[0].SoftDep[0].upgradeCode" Type="Str">{1038A887-23E1-4289-B0BD-0C4B83C6BA21}</Property>
+				<Property Name="DistPart[0].SoftDep[1].exclude" Type="Bool">false</Property>
+				<Property Name="DistPart[0].SoftDep[1].productName" Type="Str">NI Deployment Framework 2021 (64-bit)</Property>
+				<Property Name="DistPart[0].SoftDep[1].upgradeCode" Type="Str">{E0D3C7F9-4512-438F-8123-E2050457972B}</Property>
+				<Property Name="DistPart[0].SoftDep[10].exclude" Type="Bool">false</Property>
+				<Property Name="DistPart[0].SoftDep[10].productName" Type="Str">NI TDM Streaming 21.0</Property>
+				<Property Name="DistPart[0].SoftDep[10].upgradeCode" Type="Str">{4CD11BE6-6BB7-4082-8A27-C13771BC309B}</Property>
+				<Property Name="DistPart[0].SoftDep[2].exclude" Type="Bool">false</Property>
+				<Property Name="DistPart[0].SoftDep[2].productName" Type="Str">NI Error Reporting 2020 (64-bit)</Property>
+				<Property Name="DistPart[0].SoftDep[2].upgradeCode" Type="Str">{785BE224-E5B2-46A5-ADCB-55C949B5C9C7}</Property>
+				<Property Name="DistPart[0].SoftDep[3].exclude" Type="Bool">false</Property>
+				<Property Name="DistPart[0].SoftDep[3].productName" Type="Str">NI LabVIEW Real-Time NBFifo 2021</Property>
+				<Property Name="DistPart[0].SoftDep[3].upgradeCode" Type="Str">{60862FC9-172E-3FDE-A2A4-A56A76681431}</Property>
+				<Property Name="DistPart[0].SoftDep[4].exclude" Type="Bool">false</Property>
+				<Property Name="DistPart[0].SoftDep[4].productName" Type="Str">NI Logos 21.0</Property>
+				<Property Name="DistPart[0].SoftDep[4].upgradeCode" Type="Str">{5E4A4CE3-4D06-11D4-8B22-006008C16337}</Property>
+				<Property Name="DistPart[0].SoftDep[5].exclude" Type="Bool">false</Property>
+				<Property Name="DistPart[0].SoftDep[5].productName" Type="Str">NI LabVIEW Web Server 2021 (64-bit)</Property>
+				<Property Name="DistPart[0].SoftDep[5].upgradeCode" Type="Str">{5F449D4C-83B9-492E-986B-6B85A29C431D}</Property>
+				<Property Name="DistPart[0].SoftDep[6].exclude" Type="Bool">false</Property>
+				<Property Name="DistPart[0].SoftDep[6].productName" Type="Str">NI mDNS Responder 21.0</Property>
+				<Property Name="DistPart[0].SoftDep[6].upgradeCode" Type="Str">{9607874B-4BB3-42CB-B450-A2F5EF60BA3B}</Property>
+				<Property Name="DistPart[0].SoftDep[7].exclude" Type="Bool">false</Property>
+				<Property Name="DistPart[0].SoftDep[7].productName" Type="Str">Math Kernel Libraries 2017</Property>
+				<Property Name="DistPart[0].SoftDep[7].upgradeCode" Type="Str">{699C1AC5-2CF2-4745-9674-B19536EBA8A3}</Property>
+				<Property Name="DistPart[0].SoftDep[8].exclude" Type="Bool">false</Property>
+				<Property Name="DistPart[0].SoftDep[8].productName" Type="Str">Math Kernel Libraries 2020</Property>
+				<Property Name="DistPart[0].SoftDep[8].upgradeCode" Type="Str">{9872BBBA-FB96-42A4-80A2-9605AC5CBCF1}</Property>
+				<Property Name="DistPart[0].SoftDep[9].exclude" Type="Bool">false</Property>
+				<Property Name="DistPart[0].SoftDep[9].productName" Type="Str">NI VC2015 Runtime</Property>
+				<Property Name="DistPart[0].SoftDep[9].upgradeCode" Type="Str">{D42E7BAE-6589-4570-B6A3-3E28889392E7}</Property>
+				<Property Name="DistPart[0].SoftDepCount" Type="Int">11</Property>
+				<Property Name="DistPart[0].upgradeCode" Type="Str">{130967B8-62DA-3725-A46E-2E8360EA95EA}</Property>
+				<Property Name="DistPartCount" Type="Int">1</Property>
+				<Property Name="INST_author" Type="Str">University College Cork</Property>
+				<Property Name="INST_autoIncrement" Type="Bool">true</Property>
+				<Property Name="INST_buildLocation" Type="Path">../builds/IBM4/My Installer</Property>
+				<Property Name="INST_buildLocation.type" Type="Str">relativeToCommon</Property>
+				<Property Name="INST_buildSpecName" Type="Str">My Installer</Property>
+				<Property Name="INST_defaultDir" Type="Str">{E49E039F-93CE-447C-BA62-4C096924AC7C}</Property>
+				<Property Name="INST_installerName" Type="Str">install.exe</Property>
+				<Property Name="INST_productName" Type="Str">IBM4</Property>
+				<Property Name="INST_productVersion" Type="Str">1.0.1</Property>
+				<Property Name="InstSpecBitness" Type="Str">64-bit</Property>
+				<Property Name="InstSpecVersion" Type="Str">21008000</Property>
+				<Property Name="MSI_arpCompany" Type="Str">University College Cork</Property>
+				<Property Name="MSI_autoselectDrivers" Type="Bool">true</Property>
+				<Property Name="MSI_distID" Type="Str">{559E63F6-EC60-461F-86E4-759E59E21BC0}</Property>
+				<Property Name="MSI_hideNonRuntimes" Type="Bool">true</Property>
+				<Property Name="MSI_osCheck" Type="Int">0</Property>
+				<Property Name="MSI_upgradeCode" Type="Str">{20CD815E-F362-4DCA-96E6-003A1E9D1D79}</Property>
+				<Property Name="RegDest[0].dirName" Type="Str">Software</Property>
+				<Property Name="RegDest[0].dirTag" Type="Str">{DDFAFC8B-E728-4AC8-96DE-B920EBB97A86}</Property>
+				<Property Name="RegDest[0].parentTag" Type="Str">2</Property>
+				<Property Name="RegDestCount" Type="Int">1</Property>
+			</Item>
+		</Item>
+	</Item>
+</Project>
---
+++ FHP_IBM4_2022/code.py
@@ -0,0 +1,197 @@
+import supervisor
+import time
+import board
+import pwmio
+import busio # this may be needed to read data from pin D2
+import digitalio # this may be needed to read data from pin D2
+from analogio import AnalogIn
+from analogio import AnalogOut
+
+# pwm = pwmio.PWMOut(board.D9, frequency=500)
+# pwm = pwmio.PWMOut(board.D9, duty_cycle=2 ** 15, frequency=440, variable_frequency=True)
+pwm5 = pwmio.PWMOut(board.D5, duty_cycle=2 ** 15)
+pwm7 = pwmio.PWMOut(board.D7, duty_cycle=2 ** 15)
+pwm9 = pwmio.PWMOut(board.D9, duty_cycle=2 ** 15)
+pwm10 = pwmio.PWMOut(board.D10, duty_cycle=2 ** 15)
+pwm11 = pwmio.PWMOut(board.D11, duty_cycle=2 ** 15)
+pwm12 = pwmio.PWMOut(board.D12, duty_cycle=2 ** 15)
+pwm13 = pwmio.PWMOut(board.D13, duty_cycle=2 ** 15)
+
+# For listening for serial inputs
+####
+# Define names for In and Out pins
+Vout0 = AnalogOut(board.A0)
+Vout1 = AnalogOut(board.A1)
+Vin2 = AnalogIn(board.A2)
+Vin3 = AnalogIn(board.A3)
+Vin4 = AnalogIn(board.A4)
+Vin5 = AnalogIn(board.A5)
+Vin6 = AnalogIn(board.D2) # according to pinout doc pin labelled as 2 is another AI
+NRdChLim = 5 # Upper limit for no of channels that can read, previously NRdChLim = 4
+
+# Define the constants
+# For notes on the following see 
+# https://learn.adafruit.com/circuitpython-basics-analog-inputs-and-outputs/analog-to-digital-converter-inputs
+# ADC values in circuit python are all put in the range of 16-bit unsigned values so 0 - 65535 (-1+2**16)
+Vmax = Vin2.reference_voltage # max AO/AI value
+bit_scale = (64*1024) # 64 bits
+
+# Functions to convert from 12-bit to Volt.
+def dac_value(volts):
+    return int(volts / 3.3 * 65535)
+def get_voltage(pin):
+    return (pin.value * 3.3) / 65535
+def get_PWM(percentage):
+    return (int(percentage/100.0*0xffff+0.5))
+while True:
+    if supervisor.runtime.serial_bytes_available:   # Listens for a serial command
+        command = input()
+        if command.startswith("*IDN"):
+            print('ISBY-UCC-RevA.1')
+        if command.startswith("PWM"):
+            try:
+                Tokens = command[3:].split(":")
+                ThePin = int(Tokens[0])
+                SetPWM = float(Tokens[1])
+                if ThePin == 5:
+                    pwm5.duty_cycle = get_PWM(SetPWM)                
+                elif ThePin == 7:
+                    pwm7.duty_cycle = get_PWM(SetPWM)                
+                elif ThePin == 9:
+                    pwm9.duty_cycle = get_PWM(SetPWM)                
+                elif ThePin == 10:
+                    pwm10.duty_cycle = get_PWM(SetPWM)                
+                elif ThePin == 11:
+                    pwm11.duty_cycle = get_PWM(SetPWM)                
+                elif ThePin == 12:
+                    pwm12.duty_cycle = get_PWM(SetPWM)                
+                elif ThePin == 13:
+                    pwm13.duty_cycle = get_PWM(SetPWM)                
+                else:
+                    print('Pin out of range: 5,7,9,10-13')
+            except ValueError as ex:
+                print('Vset must be an integer')
+                print(ex)
+            else:
+                print("PWMset", ThePin, "=", str(SetPWM), end=' ')
+                print()
+        if command.startswith("Write"):
+            try:
+                Tokens = command[5:].split(":")
+                Chan = int(Tokens[0])
+                SetVoltage = float(Tokens[1])
+                if SetVoltage >= 0 and SetVoltage < 3.31:
+                    # Sets limits on the Output voltage to board specs
+                    if Chan == 0:
+                        Vout0.value = dac_value(SetVoltage)  # Set the voltage
+                    elif Chan == 1:
+                        Vout1.value = dac_value(SetVoltage)  # Set the voltage
+                    else:
+                        print('Channel out of range: 0 - 1')
+                else:
+                    print('Vset out of range: 0 - 3.3V')
+            except ValueError as ex:
+                print('Vset must be a float')
+                print(ex)
+            except:
+                print('Unknown problem')
+            else:
+                print("Vset", Chan, "=", str(SetVoltage), end=' ')
+                print()
+        if command.startswith("Read"):
+            try:
+                Tokens = command[4:].split(":")
+                Chan = int(Tokens[0])
+                N = int(Tokens[1])
+                if Chan == 0:
+                    Pin = Vin2
+                elif Chan == 1:
+                    Pin = Vin3
+                elif Chan == 2:
+                    Pin = Vin4
+                elif Chan == 3:
+                    Pin = Vin5
+                elif Chan == 4:
+                    Pin = Vin6
+                else:
+                    print('Channel out of range: 0 - 3')
+                if N < 1:
+                    print('Must read at least one value')
+            except ValueError as ex:
+                print('Channel must be an integer')
+                print(ex)
+            except:
+                print('Unknown problem')
+            else:
+                if Chan in range(0, NRdChLim) and N > 0:
+                    Values = [0] * N
+                    for i in range(N):
+                        Values[i] = get_voltage(Pin)
+                    print("Output", Chan, "=", str(Values[0]), end=' ')
+                    for i in range(1, N):
+                        print(",", str(Values[i]), end=' ')
+                    print()
+        if command.startswith("Average"):
+            try:
+                Tokens = command[7:].split(":")
+                Chan = int(Tokens[0])
+                N = int(Tokens[1])
+                if Chan == 0:
+                    Pin = Vin2
+                elif Chan == 1:
+                    Pin = Vin3
+                elif Chan == 2:
+                    Pin = Vin4
+                elif Chan == 3:
+                    Pin = Vin5
+                elif Chan == 4:
+                    Pin = Vin6
+                else:
+                    print('Channel out of range: 0 - 3')
+                if N < 1:
+                    print('Must average at least one value')
+            except ValueError as ex:
+                print('Channel must be an integer')
+                print(ex)
+            except:
+                print('Unknown problem')
+            else:
+                if Chan in range(0, NRdChLim) and N > 0:
+                    Value = 0.0
+                    for i in range(N):
+                        Value += get_voltage(Pin)
+                    Value /= N
+                    print("Average", Chan, "=", str(Value))
+        if command.startswith("BRead"):
+            try:
+                Tokens = command[5:].split(":")
+                Chan = int(Tokens[0])
+                N = int(Tokens[1])
+                if Chan == 0:
+                    Pin = Vin2
+                elif Chan == 1:
+                    Pin = Vin3
+                elif Chan == 2:
+                    Pin = Vin4
+                elif Chan == 3:
+                    Pin = Vin5
+                elif Chan == 4:
+                    Pin = Vin6
+                else:
+                    print('Channel out of range: 0 - 3')
+                if N < 1:
+                    print('Must read at least one value')
+            except ValueError as ex:
+                print('Channel must be an integer')
+                print(ex)
+            except:
+                print('Unknown problem')
+            else:
+                if Chan in range(0, NRdChLim) and N > 0:
+                    Values = [0] * N
+                    for i in range(N):
+                        Values[i] = Pin.value
+                    print("Output", Chan, "=", str(Values[0]), end=' ')
+                    for i in range(1, N):
+                        print(",", str(Values[i]), end=' ')
+                    print()
