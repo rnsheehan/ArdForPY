9847c3711af0ffa32d1e73db43e1868fd90c204e
Robert Sheehan <robertnsheehan@gmail.com>
2025-10-20 16:20:44 +0100

    Create uHeater.ino
    Started new sketch for writing voltage to micro-heater.

---
+++ uHeater_Ctrl/uHeater/uHeater.ino
@@ -0,0 +1,12 @@
+// Want to be able to output on all PWM outputs simultaneously
+// 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
